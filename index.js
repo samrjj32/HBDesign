f781eade87f6c5afa766066b28f05f275c1807ae
samrajj <samrajj3223@gmail.com>
2021-04-13 15:26:58 +0530

    fix

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-const text= document.getElementById("sec2-parllx-text");
-const btn= document.getElementById("sec2-parllx-btn");
+const text = document.getElementById("sec2-parllx-text");
+const btn = document.getElementById("sec2-parllx-btn");
 
 // const img= document.getElementById("onChair");
 
-
-
 document.addEventListener("scroll", function (e) {
-    let scroll = window.pageYOffset;
-   
+  let scroll = window.pageYOffset;
 
+  text.style.top = -scroll / 20 + "%";
+  // btn.style.marginLeft = (-scroll/10 ) + "%" ;
+  // img.style.right = (scroll/70 ) + "%" ;
+  // text.style.fontSize=(scroll/6)+ "px" ;
+});
 
-    text.style.top = (-scroll/20 ) + "%" ;
-    // btn.style.marginLeft = (-scroll/10 ) + "%" ;
-    // img.style.right = (scroll/70 ) + "%" ;
-    // text.style.fontSize=(scroll/6)+ "px" ;
-  });
+document.getElementById("toggle-btn").addEventListener("click", myFunction);
+
+function myFunction() {
+  var element = document.body;
+  console.log(document.getElementById("toggle-btn").checked, "nmm");
+
+  if (document.getElementById("toggle-btn").checked) {
+    trans();
+    element.classList.toggle("dark-mode");
+  } else {
+    trans();
+    element.classList.remove("dark-mode");
+  }
+
+  console.log(element.classList, "mm");
+}
+
+trans = () => {
+  document.documentElement.classList.add("transition");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+  }, 1000);
+};
